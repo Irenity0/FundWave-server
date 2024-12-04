cdb74539fd5800287c9b8b743917bb181b9285e6
Irenity <lilithkard77@gmail.com>
2024-12-05 03:26:24 +0600

    added campaign api endpoints and operations

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oo5u4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,13 +24,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    const userCollection = client.db('FundWave').collection("users");
+    const campaignCollection = client.db("FundWave").collection("campaigns");
+    
+    // campaign apis
+    app.post('/campaigns', async (req, res) => {
+      const newCampaign = req.body;
+      console.log('Adding new campaign', newCampaign)
+
+      const result = await campaignCollection.insertOne(newCampaign);
+      res.send(result);
+  });
+
+    app.get('/campaigns', async (req, res) => {
+      const cursor = campaignCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+    
+    
+
+
+  
+
+  
+
+
+
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
