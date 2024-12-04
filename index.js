a214539a6b7aea7a07708cb5fcdc8e10637b0faf
Irenity <lilithkard77@gmail.com>
2024-12-05 03:28:56 +0600

    getting single user data by email

--- index.js
+++ index.js
@@ -64,7 +64,12 @@ async function run() {
       res.send(result);
   });
 
-
+  app.get('/users/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    res.send(user);
+  });
 
 
   
